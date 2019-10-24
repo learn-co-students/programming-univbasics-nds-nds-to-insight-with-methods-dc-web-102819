dcfa685ec84100266085479a5c619abea3eacc1a
eunice kim <oonis86@gmail.com>
2019-10-24 19:36:53 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,6 +1,33 @@
-def directors_database
-	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
-  @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
+def directors_totals(nds)
+  result = {}
+  nil
+  director_index = 0
+  while director_index < nds.size do
+    director = nds[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+  result
 end
 
-p grand_total
+# Find a way to accumulate the :worldwide_grosses and return that Integer
+# using director_data as input
+def gross_for_director(director_data)
+  total = 0
+  index = 0
+
+  while index < director_data[:movies].length do
+    total += director_data[:movies][index][:worldwide_gross]
+    index += 1
+    
+grand_total += vm[row_index][column_index][inner_index][:price]
+      inner_index += 1
+    end
+    column_index += 1
+  end
+  row_index += 1
+end
+ 
+
+  total
+end
